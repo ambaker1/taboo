e092fbe75943a394c9c17bc30097471f45a20498
Alex Baker <ambaker1@mtu.edu>
2023-09-25 16:11:51 -0500

    working on tests

--- build.tcl
+++ build.tcl
@@ -6,7 +6,7 @@ tin import new from vutil
 set version 0.1
 set config ""
 dict set config VERSION $version
-dict set config VUTIL_VERSION 1.1
+dict set config VUTIL_VERSION 1.1.1
 dict set config NDLIST_VERSION 0.1
 tin bake src build $config
 tin bake doc/template/version.tin doc/template/version.tex $config
@@ -14,39 +14,64 @@ tin bake doc/template/version.tin doc/template/version.tex $config
 source build/taboo.tcl 
 namespace import taboo::*
 
-test new_table {
-    # Create a new table, in two ways
+# add           # Add keys/fields to table
+# cget          # Get column of data
+# clean         # Clean table of keys/fields with no data
+# clear         # Clear table data, keeping field names
+# cset          # Set an entire column
+# data          # Get dictionary-style data of table
+# define        # Define table properties
+# exists        # Check if keys/fields/values exist
+# expr          # Perform column operation on table
+# fedit         # Create field with expr.
+# fields        # Get list of fields given column index and glob patterns 
+# filter        # Filter table given expr
+# find          # Get row/column ID given key/field
+# get           # Get single values from table
+# height        # Get height of table (number of keys)
+# insert        # Insert keys/fields into table
+# keyname       # Get keyname
+# keys          # Get list of keys given row index and glob patterns 
+# merge         # Merge table data into current table
+# mget          # Get matrix of data from table
+# mkkey         # Make a field the key
+# move          # Move rows/columns in table
+# mset          # Set a matrix of data in table
+# query         # Query keys that meet table expr.
+# remove        # Remove keys/fields from table
+# rename        # Rename keys/fields in table
+# rget          # Get row of data in table
+# rset          # Set rows of data in table
+# search        # Search for keys meeting lsearch criteria in table
+# set           # Set single values in table
+# shape         # Get shape of table
+# sort          # Sort table using lsort
+# swap          # Swap rows/columns in table
+# transpose     # Transpose table
+# values        # Get table values
+# width         # Get width of table (number of fields)
+# wipe          # Wipe table (resets to fresh table)
+# with          # Loop through tabular data
+
+test new_table1 {
+    # Blank table (exists, but empty)
 } -body {
     ::taboo::table new tblObj
-    new table tblObj2
-    assert [$tblObj] eq [$tblObj2]
-    $tblObj2 destroy
     $tblObj info
-} -result {exists 1 shape {0 0} type table value {keyname key fieldname field keys {} fields {} data {}}}
+} -result {exists 1 shape {0 0} type table value {key {}}}
 
-test table_init_methods {
-# Single entry or dictionary entry settings
-} -body {
-    new table tbl1 keys {1 2 3}
-    new table tbl2 {keys {1 2 3}}
-    assert [$tbl1] eq [$tbl2]
-    unset tbl1 tbl2
-} -result {}
-
-test table_properties {
-    # Verify that a table is created properly
+test new_table2 {
+    # Create a table with data
 } -body {
     # Create test table (overwrite)
-    new table tblObj data {
-        1 {x 3.44 y 7.11 z 8.67}
-        2 {x 4.61 y 1.81 z 7.63}
-        3 {x 8.25 y 7.56 z 3.84}
-        4 {x 5.20 y 6.78 z 1.11}
-        5 {x 3.26 y 9.92 z 4.56}
+    new table tblObj {
+        key {1 2 3 4 5} 
+        x {3.44 4.61 8.25 5.20 3.26}
+        y {7.11 1.81 7.56 6.78 9.92}
+        z {8.67 7.63 3.84 1.11 4.56}
     }
-    assert [$tblObj] eq [$tblObj properties]
-    $tblObj properties
-} -result {keyname key fieldname field keys {1 2 3 4 5} fields {x y z} data {1 {x 3.44 y 7.11 z 8.67} 2 {x 4.61 y 1.81 z 7.63} 3 {x 8.25 y 7.56 z 3.84} 4 {x 5.20 y 6.78 z 1.11} 5 {x 3.26 y 9.92 z 4.56}}}
+    $tblObj
+} -result {key {1 2 3 4 5} x {3.44 4.61 8.25 5.20 3.26} y {7.11 1.81 7.56 6.78 9.92} z {8.67 7.63 3.84 1.11 4.56}}
 
 test copy_table_gc {
     # Test the copy functionality, and garbage collection
@@ -62,33 +87,14 @@ test copy_table_gc {
 test trim_table {
     # Use the "define" method to trim a table, and verify that it returns object
 } -body {
-    [$tblCopy define keys {1 2} fields x]
-} -result {keyname key fieldname field keys {1 2} fields x data {1 {x 3.44} 2 {x 4.61}}}
+    [[$tblCopy define keys {1 2}] define fields {x}]
+} -result {key {1 2} x {3.44 4.61}}
 
 test keyname {
     # Verify the default keyname
 } -body {
     $tblObj keyname
 } -result {key}
-
-test fieldname {
-    # Verify the default fieldname
-} -body {
-    $tblObj fieldname
-} -result {field}
-
-test key_field_rid_cid {
-    # Get key/field with row/column ID
-} -body {
-    assert [$tblObj key 0] eq 1
-    assert [$tblObj rid 1] == 0
-    assert [$tblObj key end] eq 5
-    assert [$tblObj rid 5] == 4
-    assert [$tblObj field 0] eq x
-    assert [$tblObj cid x] == 0
-    assert [$tblObj field end] eq z
-    assert [$tblObj cid z] == 2
-} -result {}
 
 test keys_fields {
     # Get keys/fields, using range notation and glob
@@ -98,9 +104,20 @@ test keys_fields {
     assert [$tblObj fields] eq {x y z}
     assert [$tblObj fields 0:1] eq {x y}
     assert [$tblObj fields end] eq {z}
-    assert [$tblObj fields : {[x-y]}] eq {x y}
 } -result {}
 
+test find {
+    # Get key/field with row/column ID
+} -body {
+    assert [$tblObj keys 0*] eq 1
+    assert [$tblObj find key 1] == 0
+    assert [$tblObj keys end*] eq 5
+    assert [$tblObj find key 5] == 4
+    assert [$tblObj fields 0*] eq x
+    assert [$tblObj find field x] == 0
+    assert [$tblObj fields end*] eq z
+    assert [$tblObj find field z] == 2
+} -result {}
 
 test rename_keys {
     # Rename keys 
@@ -116,7 +133,7 @@ test rename_keys2 {
     $tblObj --> tblCopy
     $tblCopy rename keys [$tblCopy keys 0:2] {K1 K2 K3}
     assert [$tblCopy keys] eq {K1 K2 K3 4 5}
-    $tblCopy keys : K*
+    $tblCopy search -all K*
 } -result {K1 K2 K3}
 
 test rename_fields {
@@ -125,9 +142,8 @@ test rename_fields {
     $tblObj --> tblCopy
     $tblCopy rename fields {a b c}; # Renames all fields
     $tblCopy rename fields {c a} {C A}; # Selected fields
-    assert [$tblCopy fields] eq {A b C}
-    $tblCopy fields : {[A-Z]}
-} -result {A C}
+    $tblCopy fields
+} -result {A b C}
 
 test fedit_mkkey_remove {
     # Tests for fedit, mkkey and remove 
@@ -147,7 +163,7 @@ test clear_clean_wipe {
     # tests for clear, clean, and wipe
 } -body {
     # clear, clean, and wipe
-    $tblCopy define keyname foo
+    $tblCopy keyname foo
     $tblCopy clear
     assert [$tblCopy shape] eq {0 3}
     $tblCopy clean
@@ -331,27 +347,22 @@ test add_sort {
     assert [$tblCopy keys] eq {0 1 2 3 4 5 7 8 12}
 } -result {}
 
-test rmove_rswap {
+test move_swap {
     # Move and swap rows, back to original location
 } -body {
     $tblObj --> tblCopy
-    $tblCopy rmove 1 end-1
+    $tblCopy move key 1 end-1
     assert [$tblCopy keys] eq {2 3 4 1 5}
-    $tblCopy rswap 1 5
+    $tblCopy swap keys 1 5
     assert [$tblCopy keys] eq {2 3 4 5 1}
-    $tblCopy rmove [$tblCopy key end] 0
+    $tblCopy move key [$tblCopy key end] 0
     assert [$tblCopy] eq [$tblObj]
-} -result {}
-
-
-test cmove_cswap {
-# Move and swap columns
-} -body {
-    $tblCopy cmove x end
+    
+    $tblCopy move field x end
     assert [$tblCopy fields] eq {y z x}
-    $tblCopy cmove z end
+    $tblCopy move field z end
     assert [$tblCopy fields] eq {y x z}
-    $tblCopy cswap x y
+    $tblCopy swap fields x y
     assert [$tblCopy fields] eq {x y z}
     assert [$tblCopy] eq [$tblObj]
 } -result {}
