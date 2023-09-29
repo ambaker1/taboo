3bedfe795ea242c9971a41340ccb9324b03f85fc
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-09-29 14:52:46 -0500

    added install directions for ndlist

--- install.tcl
+++ install.tcl
@@ -1,5 +1,6 @@
 package require tin 1.0
 tin depend vutil 1.1.1
+tin add -auto ndlist https://github.com/ambaker1/ndlist install.tcl
 tin depend ndlist 0.1
 set dir [tin mkdir -force taboo 0.2]
 file copy pkgIndex.tcl taboo.tcl README.md LICENSE $dir
